0d6b18935ef0197368acd2d484af3c384716d653
Ivaelo <62559591+Ivaelo@users.noreply.github.com>
2022-10-12 23:46:09 +0300

    Add files via upload

--- untitled/src/Client.java
+++ untitled/src/Client.java
@@ -3,16 +3,28 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
+import java.util.Scanner;
 
 public class Client implements  Runnable {
     private  Socket client;
     private BufferedReader in;
     private PrintWriter out;
     private boolean done;
+    private String host;
+    private int port;
+
+
+    public Client(String host,int port){
+        this.host=host;
+        this.port=port;
+    }
     @Override
     public void run() {
         try {
-            client = new Socket("127.0.0.1", 9999);
+
+
+            client = new Socket(host, port);
+            //client = new Socket("127.0.0.1", 9999);
             out = new PrintWriter(client.getOutputStream(),true);
             in = new BufferedReader(new InputStreamReader(client.getInputStream()));
             InputHandler inHandler = new InputHandler();
@@ -62,8 +74,26 @@ public class Client implements  Runnable {
         }
     }
 
-    public static void main(String[] args) {
-        Client client = new Client();
+    public static void main(String[] args) throws Exception {
+        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
+        String name = read.readLine();
+        String host;
+        int port;
+        if(name.equals("/conect")) {
+            // Enter data using BufferReader
+            BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+            // Reading data using readLine
+            host = reader.readLine();
+            BufferedReader reader2 = new BufferedReader(new InputStreamReader(System.in));
+
+            // Reading data using readLine
+            port = Integer.parseInt(reader2.readLine());
+        }
+        else{
+            throw new Exception("Trqbava da napishesh (/conect ,posle hosta i posle porta na survera za da stane)");
+        }
+        Client client = new Client(host,port);
         client.run();
     }
 }
