004ad87d295a1e8db33874519177868bb99919ca
Squidfishrl <ivo.genchev4@gmail.com>
2022-10-13 02:04:24 +0300

    server multithreading && fixes

--- homeworks/13/chat-system/src/Client/Client.java
+++ homeworks/13/chat-system/src/Client/Client.java
@@ -6,22 +6,40 @@ import java.util.Scanner;
 
 class Client {
 
-        Socket serverSocket; 
-        private String username = "";
+        private static Socket serverSocket; 
+        private static BufferedReader serverInput;  // input from server
+        private static DataOutputStream serverOutput;  // output to server
+        private String username; 
+
+        public Client() {
+                serverSocket = null;
+                username = "";
+        }
 
         public static void main(String[] args) throws Exception {
 
                 Client client = new Client();
                 getAndParseInput(client);
+                closeConnection();
+        }
 
+        public static void printSocket(String header, Socket socket, String tail) {
+                String socketInfo = socket.getInetAddress().getHostAddress() + ":" + socket.getPort();
+                System.out.println(header + socketInfo + tail);
         }
 
         private void connect(String hostname, short port) {
                try {
+                        if (serverSocket != null) {
+                                System.out.println("Error, connection already exists");
+                                return;
+                        }
+
                         serverSocket = new Socket(hostname, port);
-                        System.out.println("Established connection with " +
-                                serverSocket.getInetAddress().getHostAddress() +
-                                ":" + serverSocket.getPort());
+                        printSocket("Established connection with", serverSocket, "");
+
+                        serverInput = new BufferedReader(new InputStreamReader(serverSocket.getInputStream()));
+                        serverOutput = new DataOutputStream(serverSocket.getOutputStream());
 
                } catch (IOException error) {
                         System.out.println("IOException: " + error.getMessage());
@@ -29,23 +47,47 @@ class Client {
         }
 
         private static void getAndParseInput(Client client) {
-                Scanner scanner = new Scanner(System.in);
                 String command = "";
 
-                while(true) {
-                        command = scanner.nextLine();
+                try {
+                        Scanner scanner = new Scanner(System.in);
 
-                        switch(command) {
-                        case "/connect":
-                                client.connect("localhost", (short)1210);
-                                break;
-                        case "/quit":
-                                scanner.close();
-                                return;
-                        default:
-                                System.out.println("Error, not a command");
+                        while(true) {
+                                command = scanner.nextLine();
+
+                                switch(command) {
+                                case "/connect":
+                                        // TODO: parse and pass parameters
+                                        client.connect("localhost", (short)1210);
+                                        break;
+                                case "/quit":
+                                        serverOutput.writeBytes("/quit\n");
+                                        scanner.close();
+                                        return;
+                                default:
+                                        System.out.println("Error, not a command");
+
+                                }
 
                         }
+                } catch (IOException error) {
+                        System.out.println("IOException: " + error.getMessage());
+                        error.printStackTrace();
+
+                }
+        }
+
+        public static void closeConnection() {
+                try {
+                        serverSocket.close();
+                        serverInput.close();
+                        serverOutput.close();
+
+                        printSocket("Closed connection with ", serverSocket, "");
+                        serverSocket = null;
+                } catch (IOException error) {
+                        System.out.println("IOException: " + error.getMessage());
+                        error.printStackTrace();
 
                 }
         }
--- homeworks/13/chat-system/src/Server/Server.java
+++ homeworks/13/chat-system/src/Server/Server.java
@@ -3,17 +3,24 @@ package Server;
 import java.io.*;
 import java.net.*;
 import java.util.HashMap;
+import java.util.List;
 import java.util.ArrayList;
 
 class Server {
 
         private ServerSocket serverSocket;
-        private ArrayList clients = new ArrayList<>();
-        // private HashMap clients<String, IP>
+        private short port;
+        private List<Socket> clients;
+        // private Map clients<String, IP>
+
+        public Server(short port) {
+                this.port = port;
+                clients = new ArrayList<Socket>();
+        }
 
         public static void main(String argv[]) throws Exception {
-                Server server = new Server();
-                server.start((short)1210);
+                Server server = new Server((short) 1210);
+                server.start(server.port);
         }
 
         private void start(short serverPort) {
@@ -23,16 +30,80 @@ class Server {
 
                         while (serverSocket.isClosed() == false) {
                                 Socket clientSocket = serverSocket.accept();
-                                System.out.println("Established connection with " +
-                                                clientSocket.getInetAddress().getHostAddress() +
-                                                ":" + clientSocket.getPort());
+                                clientSocket.setSoTimeout(2 * 1000);
+                                clientSocket.setKeepAlive(true);
 
+                                printSocket("Established connection with ", clientSocket, "");
                                 clients.add(clientSocket);
+
                                 // start thread
+                                new ClientThread(clientSocket).start();
                         }
                 } catch (IOException error) {
                         System.out.println("IOException: " + error.getMessage());
                 }
         }
 
+        public static void printSocket(String header, Socket socket, String tail) {
+                String socketInfo = socket.getInetAddress().getHostAddress() + ":" + socket.getPort();
+                System.out.println(header + socketInfo + tail);
+        }
+
+
+        public class ClientThread extends Thread {
+
+                private Socket clientSocket;
+                private BufferedReader clientInput;
+                private DataOutputStream clientOutput;
+
+                public ClientThread(Socket clientSocket) {
+                        this.clientSocket = clientSocket;
+
+                        try {
+                                clientInput = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+                                clientOutput = new DataOutputStream(clientSocket.getOutputStream());
+                        } catch (IOException error) {
+                                System.out.println("IOException: " + error.getMessage());
+                                error.printStackTrace();
+                        }
+                }
+
+                public void closeConnection() {
+                        try {
+                                clientSocket.close();
+                                clientInput.close();
+                                clientOutput.close();
+                                // TODO: remove client socket from arraylist
+                        } catch (IOException error) {
+                                System.out.println("IOException: " + error.getMessage());
+                                error.printStackTrace();
+                        }
+                }
+
+                @Override
+                public void run() {
+                        String inputLine = "";  // from client 
+
+                        try {
+                                while ((inputLine = clientInput.readLine()) != null) {
+                                        if (inputLine.startsWith("/quit")) {
+                                                printSocket("Destroyed connection with", clientSocket, "");
+                                                break;
+                                        }
+                                        // TODO: more commands
+                                }
+                        } catch (SocketException error) {
+                                System.out.println("SocketException: " + error);
+                        } catch (SocketTimeoutException timeout) {
+                                printSocket("", clientSocket, " Session's expired");
+                        } catch (IOException error) {
+                                System.out.println("IOException: " + error.getMessage());
+                                error.printStackTrace();
+                        }
+                        
+
+                        clients.remove(clientSocket);
+                        closeConnection();
+                }
+        }
 }
