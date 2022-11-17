bcd1612e6c584c351da8e562373f0768ead773f5
PlamenTs18 <62559734+PlamenTs18@users.noreply.github.com>
2022-11-17 23:36:33 +0200

    add homework

---
+++ homework/21/instagram-api/src/main/java/User.java
@@ -0,0 +1,51 @@
+import java.util.ArrayList;
+
+public class User {
+    private String firstName;
+    private String lastName;
+    private String username;
+    private String password;
+    private ArrayList<Post> posts = new ArrayList<>();
+
+    public User(String firstName, String lastName, String username, String password) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.username = username;
+        this.password = password;
+    }
+
+    public void addPost(Post post) {
+        posts.add(post);
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public ArrayList<Post> getPosts() {
+        return posts;
+    }
+
+    @Override
+    public String toString() {
+        return "User {" +
+                "\n\tfirstName=\"" + firstName + '"' +
+                ", \n\tlastName=\"" + lastName + '"' +
+                ", \n\tusername=\"" + username + '"' +
+                ", \n\tpassword=\"" + password + '"' +
+                ", \n\tposts=" + posts +
+                "\n}";
+    }
+}
