311acfe0bf9af8d2d321ad4a5898d3c62736f979
Avi-Il <61373795+AI18527@users.noreply.github.com>
2022-12-01 16:38:50 +0200

    1 Politicians homework #3 - Avigeia Ilieva

---
+++ homeworks/01/politician-beans/src/main/java/com/example/politicianbeans/RightPolitician.java
@@ -0,0 +1,13 @@
+package com.example.politicianbeans;
+
+public class RightPolitician {
+    private String talk;
+
+    public RightPolitician() {
+        this.talk = "we will increase salaries";
+    }
+
+    public void call(){
+        System.out.println(talk);
+    }
+}
