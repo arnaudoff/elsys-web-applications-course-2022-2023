b7139657aef2531fb50ebb20bf22457983fcea9d
Stanislav04 <slavivanov2004@gmail.com>
2022-12-01 16:41:22 +0200

    Add politician beans homework

---
+++ homeworks/23/politician-beans/src/main/java/com/example/politician_beans/PoliticianBeansApplication.java
@@ -0,0 +1,13 @@
+package com.example.politician_beans;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+@SpringBootApplication
+public class PoliticianBeansApplication {
+
+	public static void main(String[] args) {
+		new ClassPathXmlApplicationContext("beans.xml");
+	}
+
+}
