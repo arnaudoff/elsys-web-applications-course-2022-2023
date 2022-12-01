b5e6e67ee0ef426e54c4c78f3de538e7f3992f96
DBozhkovV <73282224+DBozhkovV@users.noreply.github.com>
2022-12-01 23:59:07 +0200

    Add files via upload

---
+++ homeworks/7/politician-beans/src/main/java/com/example/politician/PoliticianApplication.java
@@ -0,0 +1,14 @@
+package com.example.politician;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+@SpringBootApplication
+public class PoliticianApplication {
+
+    public static void main(String[] args) {
+        new ClassPathXmlApplicationContext("beans.xml");
+    }
+
+}
---
+++ homeworks/7/politician-beans/src/main/java/com/example/politician/RightPolitician.java
@@ -0,0 +1,8 @@
+package com.example.politician;
+
+public class RightPolitician implements Politician{
+    @Override
+    public void talk() {
+        System.out.println("we will increase salaries");
+    }
+}
