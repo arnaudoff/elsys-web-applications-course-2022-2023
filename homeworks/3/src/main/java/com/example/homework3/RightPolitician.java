1f457a78ce25307314e276e8e40f77c083295991
AlexGechovski <48589371+AlexGechovski@users.noreply.github.com>
2022-12-02 00:17:11 +0200

    3 Politicians homework #3 - Aleks Gechovski

---
+++ homeworks/3/src/main/java/com/example/homework3/RightPolitician.java
@@ -0,0 +1,8 @@
+package com.example.homework3;
+
+public class RightPolitician implements Politician{
+    @Override
+    public void talk() {
+        System.out.println("We will increase salaries!!!");
+    }
+}
