9826d0836c3c5eb001470e695cb39926e39cf59c
LachezarLazarov <62560048+LachezarLazarov@users.noreply.github.com>
2022-12-02 11:06:07 +0200

    Add files via upload

---
+++ homeworks/18/politician-beans/src/main/java/com/politicianbeans/LeftPolitician.java
@@ -0,0 +1,7 @@
+package com.politicianbeans;
+
+public class LeftPolitician {
+    public void talk() {
+        System.out.println("we will increase pensions");
+    }
+}
---
+++ homeworks/18/politician-beans/src/main/java/com/politicianbeans/RightPolitician.java
@@ -0,0 +1,7 @@
+package com.politicianbeans;
+
+public class RightPolitician {
+    public void talk() {
+        System.out.println("we will increase salaries");
+    }
+}
