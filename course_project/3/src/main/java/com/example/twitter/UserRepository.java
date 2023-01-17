4c4ab85bec0d5e9ad8ad373c96acd56b5ae8043d
AlexGechovski <48589371+AlexGechovski@users.noreply.github.com>
2023-01-17 22:41:47 +0200

    Delete course_project/3 directory

--- course_project/3/src/main/java/com/example/twitter/UserRepository.java
+++
@@ -1,6 +0,0 @@
-package com.example.twitter;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-    User findByUsername(String username);
-}
