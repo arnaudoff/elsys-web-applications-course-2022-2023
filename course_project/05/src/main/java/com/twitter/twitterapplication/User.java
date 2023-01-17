6557f692ef93dbd99c0eea5b888022a5ddf62b37
Boryana Stefanova <47723617+Boryana23@users.noreply.github.com>
2023-01-17 22:37:45 +0200

    Add files via upload

---
+++ course_project/05/src/main/java/com/twitter/twitterapplication/User.java
@@ -0,0 +1,51 @@
+package com.twitter.twitterapplication;
+
+import java.util.Date;
+
+public class User {
+    private int id;
+    private String username;
+    private Date dateRegistered;
+    private long followersCount;
+    private long followingCount;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public Date getDateRegistered() {
+        return dateRegistered;
+    }
+
+    public void setDateRegistered(Date dateRegistered) {
+        this.dateRegistered = dateRegistered;
+    }
+
+    public long getFollowersCount() {
+        return followersCount;
+    }
+
+    public void setFollowersCount(long followersCount) {
+        this.followersCount = followersCount;
+    }
+
+    public long getFollowingCount() {
+        return followingCount;
+    }
+
+    public void setFollowingCount(long followingCount) {
+        this.followingCount = followingCount;
+    }
+}
